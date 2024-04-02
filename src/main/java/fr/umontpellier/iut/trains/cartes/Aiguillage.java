6b73b472ed3077510fb0d01cac49c9e0e48cc834
Desal-42 <alexandre.deschanel.666@gmail.com>
2024-04-02 15:22:45 +0200

    Ajout méthode pioché dans Aiguillage

--- src/main/java/fr/umontpellier/iut/trains/cartes/Aiguillage.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Aiguillage.java
@@ -1,7 +1,19 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class Aiguillage extends Carte {
     public Aiguillage() {
         super("Aiguillage", 5,0);
+    }
+
+
+    /**
+     * Effet : ET JE PIOCHE, DRAGON BLANC AUX YEUX BLEU
+     * @param joueur
+     */
+    @Override
+    public void jouer(Joueur joueur) {
+        joueur.piocher(2);
     }
 }
