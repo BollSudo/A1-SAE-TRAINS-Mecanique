d448bf72931de5e2595dbe8353ac661385207972
Desal-42 <alexandre.deschanel.666@gmail.com>
2024-04-25 07:06:34 +0200

    implémentation de la fonction jouer dans FeuDeSignalisation

--- src/main/java/fr/umontpellier/iut/trains/cartes/FeuDeSignalisation.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/FeuDeSignalisation.java
@@ -1,7 +1,31 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Bouton;
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.Arrays;
+import java.util.List;
+
+
 public class FeuDeSignalisation extends CarteAction {
     public FeuDeSignalisation() {
         super("Feu de signalisation", 2, 0);
+    }
+
+    @Override
+    public void jouer(Joueur joueur){
+        joueur.ajouterAlaMain(joueur.piocher());
+        Carte carte = joueur.piocher();
+        Bouton boutonOui = new Bouton("oui", "oui");
+        Bouton boutonNon = new Bouton("non", "non");
+        List<Bouton> boutons = Arrays.asList(boutonOui, boutonNon);
+
+        String rep = joueur.choisir("Voulez vous défaussez la carte ?", null, boutons, false);
+
+        if (rep == "oui"){
+            joueur.ajouterCarteDansDefausse(carte);
+        }else {
+            joueur.ajouterCarteSurPioche(carte);
+        }
     }
 }
