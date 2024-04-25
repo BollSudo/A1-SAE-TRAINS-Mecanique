8a4242d97c9969932f4c8f550a85a414e75198ae
Desal-42 <alexandre.deschanel.666@gmail.com>
2024-04-25 19:13:02 +0200

    implémentation de la fonction jouer dans PersonnelDeGare

--- src/main/java/fr/umontpellier/iut/trains/cartes/PersonnelDeGare.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/PersonnelDeGare.java
@@ -1,7 +1,33 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Bouton;
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class PersonnelDeGare extends CarteAction {
     public PersonnelDeGare() {
         super("Personnel de gare", 2, 0);
     }
+
+    @Override
+    public void jouer(Joueur joueur){
+        Bouton boutonPioche = new Bouton("piocher", "piocher");
+        Bouton boutonFerraille = new Bouton("ferraille", "ferraille");
+        Bouton boutonArgent = new Bouton("argent", "argent");
+        List<Bouton> boutons = Arrays.asList(boutonPioche, boutonFerraille, boutonArgent);
+
+        String rep = joueur.choisir("Choisissez un parmi les trois :", null, boutons, false);
+
+        if (rep == "piocher"){
+            joueur.ajouterAlaMain(joueur.piocher());
+        } else if (rep == "ferraille"){
+            Carte c = joueur.getMain().retirer("Ferraille");
+            joueur.getJeu().getReserve().get("Ferraille").add(c);
+        } else {
+            joueur.incrementerArgent(1);
+        }
+    }
+    
 }
