7a88be3a1ab777cf82bda447265fc69a52fe7ac1
Desal-42 <alexandre.deschanel.666@gmail.com>
2024-04-25 19:31:05 +0200

    implémentation de la fonction jouer dans Parc d'attractions

--- src/main/java/fr/umontpellier/iut/trains/cartes/ParcDAttractions.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/ParcDAttractions.java
@@ -1,7 +1,27 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class ParcDAttractions extends CarteAction {
     public ParcDAttractions() {
         super("Parc d'attractions", 4, 1);
+    }
+
+    @Override
+    public void jouer(Joueur joueur){
+        super.jouer(joueur);
+        ListeDeCartes cartes = joueur.getCartesEnJeu();
+        Collection<String> cartesValide = new ArrayList<>();
+        for (Carte carte : cartes){
+            if (carte.estDeType(TypeCarte.TRAIN)){
+                cartesValide.add(carte.getNom());
+            }
+        }
+        String carteChoisi = joueur.choisir("Choisissez une carte Train", cartesValide, null, false);
+        int value = cartes.getCarte(carteChoisi).getValeur();
+        joueur.incrementerArgent(value);
     }
 }
