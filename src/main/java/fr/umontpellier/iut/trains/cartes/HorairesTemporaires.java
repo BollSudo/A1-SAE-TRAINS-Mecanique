26a1b0c8fd6a0ac87a922b293a9bdfc470703eec
Desal-42 <alexandre.deschanel.666@gmail.com>
2024-04-26 06:41:23 +0200

    implémentation de la fonction jouer dans HorairesTemporaires

--- src/main/java/fr/umontpellier/iut/trains/cartes/HorairesTemporaires.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/HorairesTemporaires.java
@@ -1,7 +1,25 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class HorairesTemporaires extends CarteAction {
     public HorairesTemporaires() {
         super("Horaires temporaires", 5, 0);
+    }
+
+    @Override
+    public void jouer(Joueur joueur){
+        int train = 0;
+        Carte carte;
+        while (train < 2){
+            carte = joueur.piocher();
+            if (carte.estDeType(TypeCarte.TRAIN)){
+                train = train + 1;
+                joueur.ajouterAlaMain(carte);
+            }
+            else {
+                joueur.ajouterCarteDansDefausse(carte);
+            }
+        }
     }
 }
